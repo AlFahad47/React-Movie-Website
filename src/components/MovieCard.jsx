92a7db71fb267799d4d70adf86d6b17cd804ac7b
MD Fahad Molla <ahfahad47sss@gmail.com>
2024-12-20 22:05:07 +0600

    base code

---
+++ src/components/MovieCard.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const MovieCard = () => {
+  return (
+    <div className='movie-card'>
+        <div className='movie-poster'>
+            <img src={moive.title}/>
+        </div>
+    </div>
+  )
+}
+
+export default MovieCard
