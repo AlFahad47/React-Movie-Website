294b799d750ddb397e4701b556fdf49bc2d6a502
MD Fahad Molla <ahfahad47sss@gmail.com>
2024-12-22 20:30:23 +0600

    created movieContext file

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,20 @@ import './App.css'
 import Home from './pages/Home'
 import Favorites from './pages/Favorites'
 import Navbar from './components/Navbar'
+import { MovieProvider } from './contexts/MovieContext'
 
 function App() {
 
 
   return (
 
-    <div>
+    <MovieProvider>
     <Navbar/>
     <Routes>
       <Route path='/' element={<Home/>}/>
       <Route path='/favorites' element={<Favorites/>} />
     </Routes>
-    </div> 
+    </MovieProvider> 
 
   )
 }
