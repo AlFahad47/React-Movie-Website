c3c9147a0c2858e37d99bbf303d36c44968992b2
MD Fahad Molla <ahfahad47sss@gmail.com>
2024-12-25 20:03:30 +0600

    home page created without search option

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,46 @@
 import React from 'react'
+import MovieCard from '../components/MovieCard'
+import { useState,useEffect } from 'react'
+import { searchMovies,getPopularMovies } from '../services/api'
+import '../css/Home.css'
 
 const Home = () => {
+  const [error, setError] = useState(null);
+  const [loading,setLoading] = useState(false);
+  const [movies,setMovies] = useState([]);
+
+  useEffect(()=>{
+    const loadPopularMovies = async  ()=>{
+      try{
+        const popluarMovies = await getPopularMovies();
+        setMovies(popluarMovies);
+      } catch(err) {
+        console.log(err);
+        setError("Failed to load movies...");
+      } finally {
+        setLoading(false);
+      }
+    };
+    
+    loadPopularMovies(); 
+  },[]);
+
   return (
-    <div>Home</div>
+    <div className='home'>
+      {error && <div className='error-message'>{error}</div>}
+      { loading? (
+          <div className='loading'>Loading...</div>
+        ):(
+          <div className='movies-grid'>
+            {
+              movies.map(movie =>(
+                <MovieCard movie={movie} key={ movie.id} />
+              ))
+            }
+          </div>
+        )
+      }
+    </div>
   )
 }
 
